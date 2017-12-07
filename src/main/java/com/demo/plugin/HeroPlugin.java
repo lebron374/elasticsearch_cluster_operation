d8688fd56d5fe9f5f764cdd9434ee4a59d2b0bbb
zhi.wang <zhi.wang@husor.com>
2017-12-07 18:44:36 +0800

    修改别名

--- src/main/java/com/demo/plugin/HeroPlugin.java
+++ src/main/java/com/demo/plugin/HeroPlugin.java
@@ -3,22 +3,19 @@ package com.demo.plugin;
 import com.demo.action.HeroAction;
 import com.demo.action.TransportNodesHeroAction;
 import com.demo.module.HeroModule;
-import com.demo.rest.RestHeroAction;
 import com.google.common.collect.Lists;
 import org.elasticsearch.action.ActionModule;
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.plugins.AbstractPlugin;
-import org.elasticsearch.rest.RestModule;
 import org.elasticsearch.script.ScriptModule;
 
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * Created by zhi.wang on 2017/12/6.
  */
-public class HerePlugin extends AbstractPlugin {
+public class HeroPlugin extends AbstractPlugin {
 
     public String name() {
         return "hero";
